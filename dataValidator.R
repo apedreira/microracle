f3c31ee01a3db3670d30f2a31fc6e2547314f6d0
apedreira <52757793+apedreira@users.noreply.github.com>
2024-09-16 18:29:35 +0200

    Update dataValidator.R

--- dataValidator.R
+++ dataValidator.R
@@ -5,33 +5,29 @@ dataValidatorSingle <- function(data) {
   time_length = length(unlist(data[[1]])[!is.na(unlist(data[[1]]))])
   u = unlist(data[[2]][1])
   
-  
   c_length = length(unlist(data[[3]])[!is.na(unlist(data[[3]]))])
 
-  
   if(u == 0 || is.na(u)) {
     msg= showNotification(
-      "Initial cell concentration must be provided. Check the data from the first row of column nº2.",
+      "Initial cell concentration must be provided. Check the data from the
+      first row of column nº2.",
       type= "error")
-
   }
   
   else if(time_length != c_length){
     msg=showNotification(
-      "Please, provide a value of drug concentration for each experimental time. Check the data from the column nº3",
+      "Please, provide a value of drug concentration for each experimental time. 
+      Check the data from the column nº3",
       type= "error"
     )
-
   }
-  
   
   else  {
     msg = "OK"
-  }
-  
-  return(msg)
-  
+  }  
+  return(msg)  
 }
+
 
 dataValidatorMultiple <- function(data) {
   
@@ -53,11 +49,9 @@ dataValidatorMultiple <- function(data) {
     
     #Remove empty (NA) columns from dataframe
     
-    if(!is.null(emptyCols)) {
-      
+    if(!is.null(emptyCols)) {      
       subsetDf = subset(subsetDf, select = -emptyCols)
     }
-    
 
     #Extract columns to compare from dataframe
     time_length = length(unlist(subsetDf[[1]])[!is.na(unlist(subsetDf[[1]]))])
@@ -66,24 +60,27 @@ dataValidatorMultiple <- function(data) {
     #colNumber = ncol(subsetDf)/2-1
     colNumber = ncol(subsetDf)/2
     print (colNumber)
-    #A bucle which iterates over dataframe 1:5 is for the number of columns (10), but must be
+    #A bucle which iterates over dataframe 1:5 is for the number of columns 
+    #(10), but must be
     for (i in 1:colNumber){
       print (i)
       #Extract columns and values to compare from dataframe
       initial_u = unlist(subsetDf[[(2*i)]][1])
-      c_length = length(unlist(subsetDf[[(2*i+1)]])[!is.na(unlist(subsetDf[[(2*i+1)]]))])
-
+      c_length = length(unlist(subsetDf[[(2*i+1)]])
+                        [!is.na(unlist(subsetDf[[(2*i+1)]]))])
       if(initial_u == 0 || is.na(initial_u)) {
         if (i==1){
         msg= showNotification(
-          paste("Initial cell concentration must be provided. Check the data from the column",(i*2),"."),
+          paste("Initial cell concentration must be provided. Check the data
+                from the column",(i*2),"."),
           type= "error")
           break
         }
         
         else {
           msg= showNotification(
-          paste("Initial cell concentration must be provided. Check the data from the column",(i*2),"."),
+          paste("Initial cell concentration must be provided. Check the data
+                from the column",(i*2),"."),
           type= "error")
         break
       }
@@ -91,7 +88,9 @@ dataValidatorMultiple <- function(data) {
       }
       else if(time_length != c_length){
         msg = showNotification(
-          paste("Please, provide a value of drug concentration for each experimental time. Check the data from the column nº",(i*2+1),"."),
+          paste("Please, provide a value of drug concentration for each
+                experimental time. Check the data from the column nº",
+                (i*2+1),"."),
           type = "error"
         )
         break
@@ -102,8 +101,7 @@ dataValidatorMultiple <- function(data) {
       }
 
     }
-  }
-  
+  }  
   return(list(msg,subsetDf))
 }
 
@@ -118,16 +116,16 @@ getEmptyColumns <- function(data) {
       }
   }
   
-    if(length(emptyCols) > 0) {
+  if(length(emptyCols) > 0) {
     
-    # Remove odd index from emptyCols vector (cell concentrations has even index)
-    # despite be odd, 1 is keep to assure that Time col exist
-    # If the value is different from 1, the result of adding 1 to the current value
-    # must be added to the vector. In this way, the CFU/mL columns and their respective
-    # antimicrobial concentration columns will be eliminated from the dataframe.
+    # Remove odd index from emptyCols vector (cell concentrations has even 
+    #  index) despite be odd, 1 is keep to assure that Time col exist
+    # If the value is different from 1, the result of adding 1 to the current 
+    #  value must be added to the vector. In this way, the CFU/mL columns and 
+    # their respective antimicrobial concentration columns will be eliminated
+    # from the dataframe.
     
     for (i in 1:length(emptyCols)) {
-      
       col_index = emptyCols[i]
       
       if (col_index != 1 && col_index %% 2 == 0 ) {
