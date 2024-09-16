c0bf2b152aa94fa804c553ffc92ba9bf51f76f60
apedreira <52757793+apedreira@users.noreply.github.com>
2024-09-16 18:45:45 +0200

    Update openModal.R

--- openModal.R
+++ openModal.R
@@ -12,7 +12,8 @@ openModal = function(id) {
                column(width=6,
                       textInput("colHeader1", "Header 1:", value = "Exp1"),
                       textInput("colHeader2", "Header 2:", value = "Exp2"),
-                      textInput("colHeader3", "Header 3:", value = "Exp3")  ),
+                      textInput("colHeader3", "Header 3:", value = "Exp3")
+                      ),
                column(width=6,
                       textInput("colHeader4", "Header 4:", value = "Exp4"),
                       textInput("colHeader5", "Header 5:", value = "Exp5"),
