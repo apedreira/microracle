3eb83794ab4b02ee328a9e2f2cc2b768fd066856
Miriam R. Garcia <miriamr@iim.csic.es>
2026-02-17 12:09:09 +0100

    fix menus tabs and avoid warnings

--- app.R
+++ app.R
@@ -42,110 +42,63 @@ ui <-
     ),
     id = "navbar",
     theme = shinytheme("yeti"),
-    # Themes impede the type argument in Shiny shownotification
-    # therefore, css must be provided to solve the problem and change colors
-    tags$head(
-      # Favicon
-      tags$link(rel = "shortcut icon", href = "/icon.ico"),
-      # Necessary for avoid notifications stay so more time, float right rrss
-      # icons in navbar and aligned sub and superscripts
-
-      tags$style(
-        HTML(
-          ".shiny-notification {
-             position:fixed;
-             top: calc(50%);
-             left: calc(0%);
-             color:black;
-             }",
-          ".shiny-notification-warning {
-               color:#8f6830;
-               background-color: #fff3cd;
-
-             }",
-          ".shiny-notification-error {
-               color:#751f26;
-               background-color: #f8d7da;
-
-             }",
-          "label {margin-bottom:10px, font-weight:bold}",
-          ".control-label { font-size:110%;}",
-          ".action-button {padding:8px 16px; margin-right: 16px;font-size:120%}",
-          ".navbar-nav {float: none !important;}",
-          ".navbar-nav > li:nth-child(4) {float: left;, margin-top: 10px}",
-          ".navbar-nav > li:nth-child(5) {float: right;, margin-top: 10px}",
-          ".navbar-nav > li:nth-child(6) {float: right;, margin-top: 10px}",
-          ".navbar-nav > li:nth-child(5) a:nth-child(1) {display:none}",
-          ".navbar-nav > li:nth-child(6) a:nth-child(1) {display:none}",
-          ".nobr {white-space: nowrap;}",
-          ".navbar-default {
-            background-color:black;
-            box-sizing: border-box;
+    collapsible = TRUE,
+    
+    # HEADER: Aquí gestionamos los metadatos y los iconos directos
+    header = tagList(
+      tags$head(
+        tags$link(rel = "shortcut icon", href = "/icon.ico"),
+        tags$style(
+          HTML(
+            ".shiny-notification { position:fixed; top: 50%; left: 0%; color:black; }",
+            ".shiny-notification-warning { color:#8f6830; background-color: #fff3cd; }",
+            ".shiny-notification-error { color:#751f26; background-color: #f8d7da; }",
+            
+            # Ajuste de la barra para que acepte floats
+            ".navbar-nav { float: none !important; width: 100%; }",
+            
+            # Estilo para los iconos inyectados a la derecha
+            ".nav-icon-link { float: right !important; margin-top: 10px; margin-left: 15px; }",
+            ".nav-icon-link a { padding: 10px 15px !important; background: transparent !important; }",
+            
+            ".navbar-default { background-color:black; border: none; }",
+            ".navbar-nav > li > a { color:red; background-color:black; }",
+            
+            # Hover y activo para About y Models (hijos 1 y 2)
+            ".navbar-nav > li.active > a,
+             .navbar-nav > li:hover > a {
+               border-bottom: 8px solid #0fc1a3 !important;
+               box-sizing: border-box;
             }",
-          ".navbar-nav > li > a {
-            color:red;
-            background-color:black;
-             box-sizing: border-box;
-            }",
-          ".navbar-nav > li.active > a,
-            .navbar-nav > li.dropdown:hover > a,
-            .navbar-nav > li:nth-child(7):hover > a{
-             border-bottom: 8px solid #0fc1a3;
-             box-sizing: border-box;
-            }",
-          ".dropdown-menu li {
-            background-color:black;
-            box-sizing: border-box;
-            }",
-          ".dropdown-menu li.active,
-            .dropdown-menu li:hover {
-            border-right: 8px solid #0fc1a3;
-            }",
-          ".supsub {
-              display: inline-block;
-              margin: -9em 0;
-              vertical-align: -0.55em;
-              line-height: 1.35em;
-              font-size: 70%;
-              text-align: left;
-            }",
-          "input[name='strainID'] + span {
-              font-style: italic;
-            }",
-          ".svgIcon {
-            filter: brightness(0) invert(1);
-            height: 25px;
-            width: 25px
-            }",
-          # "#navbar > li:nth-child(2) > a:nth-child(1) {
-          # display: none;
-          # }",
-          # "#navbar > li:nth-child(3) > a:nth-child(1) {
-          #  display: none;
-          # }"
-        )
+            
+            ".svgIcon { filter: brightness(0) invert(1); height: 25px; width: 25px; }"
+          )
+        ),
+        # Script para mover los iconos al final de la navbar de forma limpia
+        tags$script(HTML("
+          $(document).ready(function() {
+            $('.navbar-nav').append($('#nav-icons').contents());
+          });
+        "))
+      ),
+      shinyjs::useShinyjs(),
+      
+      # Contenedor oculto con los links directos que moveremos con JS
+      tags$div(id = "nav-icons", style = "display:none;",
+               tags$li(class = "nav-icon-link",
+                       tags$a(href = "https://github.com/apedreira/microracle/", target = "_blank",
+                              tags$img(class = "svgIcon", src = "github.svg")
+                       )
+               ),
+               tags$li(class = "nav-icon-link",
+                       tags$a(href = "https://twitter.com/microracle", target = "_blank",
+                              tags$img(class = "svgIcon", src = "x-twitter.svg")
+                       )
+               )
       )
     ),
-    shinyjs::useShinyjs(),
-    collapsible = TRUE,
-    navbarMenu(
-      "MODELS",
-      model1UI(),
-      model2UI()
-    ),
-    #To uncomment once the new models have been included
-    navbarMenu(
-      "MODELS",
-      #  model3UI(),
-      # model4UI()
-    ),
-    tabPanel(tags$a(tags$img(class = "svgIcon", src = "x-twitter.svg"),
-      href = "https://twitter.com/microracle", target = "_blank"
-    ), ),
-    tabPanel(tags$a(tags$img(class = "svgIcon", src = "github.svg"),
-      href = "https://github.com/apedreira/microracle/",
-      target = "_blank"
-    ), ),
+    
+    # 1. About us (Pestaña simple a la izquierda)
     tabPanel(
       "About us",
       mainPanel(
@@ -154,38 +107,25 @@ ui <-
         column(
           width = 6,
           h1("Microracle"),
-          # HMTL("
-          #   <p>Microracle allows users to employ the bacterial inactivation models developed in our group with their own experimental data. Alternatively, the user can simply load the available experimental data for <i>Escherichia coli</i> and <i>Bacillus cereus</i> and modify the model parameters to test how they affect the model output.
-          # Currently, the following models are available:</p>"),
           h2("Authoring"),
-          p(
-            "Microracle has been developed by researchers of the Biosystems and Bioprocess Engineering Group (Bio2Eng) and Recycling and Valorization of Waste Materials (REVAL) groups from the IIM-CSIC (Vigo, Spain):"
-          ),
+          p("Microracle has been developed by researchers of the Biosystems and Bioprocess Engineering Group (Bio2Eng) and Recycling and Valorization of Waste Materials (REVAL) groups from the IIM-CSIC (Vigo, Spain):"),
           tags$div(tags$ul(
-            tags$li(
-              tags$span("Adrián Pedreira: Web developer, experimentation and data collection.")
-            ),
+            tags$li(tags$span("Adrián Pedreira: Web developer, experimentation and data collection.")),
             tags$li(tags$span("Nerea Martínez: Development of mathematical models.")),
             tags$li(tags$span("Xosé A. Vázquez: Experimental design")),
             tags$li(tags$span("Míriam R. García: Development of mathematical models."))
           )),
-          HTML("<p>For any question related to the mathematical models,
-                please contact with Míriam R. García (miriamr@iim.csic.es).
-                Microracle is a project in early development, so you may
-                encounter some problems on the web. To report any bug or a
-                problem, please generate a new issue in the
-                <a href='https://github.com/apedreira/microracle'target='_blank'
-                >GitHub repository</a> or contact with Adrián Pedreira
-                (apedreira@iim.csic.es).</p>"),
-          h2("Requirements"),
-          HTML("<p>Microracle can be freely accessed from
-              <a href='https://microracle.shinyapps.io/Microracle/'
-              target='_blank'>microracle.shinyapps.io/Microracle</a>.
-              No requirements other than internet connection from a web browser
-              are necessary.</p>")
+          HTML("<p>For any question related to the mathematical models, please contact with Míriam R. García (miriamr@iim.csic.es).</p>")
         )
       ),
       column(width = 3, "")
+    ),
+    
+    # 2. MODELS (Menú desplegable a la izquierda)
+    navbarMenu(
+      "MODELS",
+      model1UI(),
+      model2UI()
     )
   )
 
